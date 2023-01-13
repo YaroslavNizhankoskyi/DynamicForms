80ed068db5efa51c7c5a2d9218607a25f862fe25
Your Name <you@example.com>
2023-01-13 15:08:51 +0300

    add swagger jwt helper

---
+++ DynamicForms.API/Web/Helpers/SwaggerHeaderExtension.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Web.Helpers
+{
+    public static class SwaggerHeaderExtension
+    {
+        public static SwaggerGenOptions AddBearerTokenSupport(this SwaggerGenOptions options)
+        {
+            OpenApiSecurityScheme securityDefinition = new OpenApiSecurityScheme()
+            {
+                Name = "Bearer",
+                BearerFormat = "JWT",
+                Scheme = "bearer",
+                Description = "Specify the authorization token.",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.Http,
+            };
+            options.AddSecurityDefinition("jwt_auth", securityDefinition);
+
+            // Make sure swagger UI requires a Bearer token specified
+            OpenApiSecurityScheme securityScheme = new OpenApiSecurityScheme()
+            {
+                Reference = new OpenApiReference()
+                {
+                    Id = "jwt_auth",
+                    Type = ReferenceType.SecurityScheme
+                }
+            };
+            OpenApiSecurityRequirement securityRequirements = new OpenApiSecurityRequirement()
+            {
+                {securityScheme, new string[] { }},
+            };
+            options.AddSecurityRequirement(securityRequirements);
+
+            return options;
+        }
+    }
+}
